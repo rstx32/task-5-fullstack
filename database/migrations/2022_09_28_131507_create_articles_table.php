d7501aec47197a29dbb8c969eb0bb17ebd0f3df2
restu indera <rstx32@gmail.com>
2022-09-29 06:24:16 +0700

    change foreignId to foreignIdFor

--- database/migrations/2022_09_28_131507_create_articles_table.php
+++ database/migrations/2022_09_28_131507_create_articles_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Category;
 
 return new class extends Migration
 {
@@ -20,8 +22,8 @@ return new class extends Migration
             $table->string('title');
             $table->text('content');
             $table->binary('image');
-            $table->foreignId('user_id');
-            $table->foreignId('category_id');
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Category::class);
             $table->timestamps();
         });
     }
--- database/migrations/2022_09_28_131526_create_categories_table.php
+++ database/migrations/2022_09_28_131526_create_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -18,7 +19,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('user_id');
+            $table->foreignIdFor(User::class);
             $table->timestamps();
         });
     }
