df798ed7794e617baa5dd8f49e5c91adcf9738b7
restu indera <rstx32@gmail.com>
2022-09-29 14:47:08 +0700

    add user id

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
@@ -17,7 +18,8 @@ class CategoryFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->text()
+            'name' => $this->faker->text(),
+            'user_id' => $this->faker->unique()->numberBetween(1, User::count()),
         ];
     }
 }
