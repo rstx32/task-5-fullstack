70a0d40f3f7161b00c3c9dd754a2910228421c0c
restu indera <rstx32@gmail.com>
2022-09-30 19:39:26 +0700

    prettier

--- app/Http/Controllers/API/ArticleController.php
+++ app/Http/Controllers/API/ArticleController.php
@@ -11,64 +11,64 @@ use Validator;
 class ArticleController extends Controller
 {
     // display all article with pagination
-    public function index(){
+    public function index()
+    {
         return Article::paginate();
     }
-    
+
     // display single article
-    public function show($id){
+    public function show($id)
+    {
         $article = Article::find($id);
 
-        if($article==null){
-            return response()->json('Data Not Found!', 404);
-        }else{
-            return response()->json([
-                new ArticleResource($article)
-            ]);
+        if ($article == null) {
+            return response()->json("Data Not Found!", 404);
+        } else {
+            return response()->json([new ArticleResource($article)]);
         }
     }
 
     // create new article
-    public function store(Request $request){
-        $validator = Validator::make($request->all(),
-        [
-            'title' => 'required|string|max:255',
-            'content' => 'required|string',
-            'image' => 'required|URL',
-            'user_id' => 'required|integer',
-            'category_id' => 'required|integer',
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "title" => "required|string|max:255",
+            "content" => "required|string",
+            "image" => "required|URL",
+            "user_id" => "required|integer",
+            "category_id" => "required|integer",
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json($validator->errors());
         }
 
         $article = Article::create([
-            'title' => $request->title,
-            'content' => $request->content,
-            'image' => $request->image,
-            'user_id' => $request->user_id,
-            'category_id' => $request->category_id,
+            "title" => $request->title,
+            "content" => $request->content,
+            "image" => $request->image,
+            "user_id" => $request->user_id,
+            "category_id" => $request->category_id,
         ]);
 
         return response()->json([
-            'success creating new article',
-            new ArticleResource($article)
+            "success creating new article",
+            new ArticleResource($article),
         ]);
     }
 
     // edit an article
-    public function update(Request $request, Article $article){
-        $validator = Validator::make($request->all(),
-        [
-            'title' => 'required|string|max:255',
-            'content' => 'required|string',
-            'image' => 'required|URL',
-            'user_id' => 'required|integer',
-            'category_id' => 'required|integer',
+    public function update(Request $request, Article $article)
+    {
+        $validator = Validator::make($request->all(), [
+            "title" => "required|string|max:255",
+            "content" => "required|string",
+            "image" => "required|URL",
+            "user_id" => "required|integer",
+            "category_id" => "required|integer",
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json($validator->errors());
         }
 
@@ -80,15 +80,16 @@ class ArticleController extends Controller
         $article->save();
 
         return response()->json([
-            'article updated',
-            new ArticleResource($article)
+            "article updated",
+            new ArticleResource($article),
         ]);
     }
 
     // delete an article
-    public function destroy(Article $article){
+    public function destroy(Article $article)
+    {
         $article->delete();
 
-        return response()->json('article deleted');
+        return response()->json("article deleted");
     }
 }
--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -10,53 +10,58 @@ use Validator;
 class PassportController extends Controller
 {
     // login
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $login = $request->validate([
-            'email' => 'required',
-            'password' => 'required'
+            "email" => "required",
+            "password" => "required",
         ]);
 
-        if(!Auth::attempt($login)){
-            $msg = 'invalid credential!';
+        if (!Auth::attempt($login)) {
+            $msg = "invalid credential!";
             return response()->json($msg);
         }
 
-        $accessToken = Auth::user()->createToken('accessToken')->accessToken;
+        $accessToken = Auth::user()->createToken("accessToken")->accessToken;
 
         return response()->json([
-            'user' => Auth::user(),
-            'access_token' => $accessToken,
+            "user" => Auth::user(),
+            "access_token" => $accessToken,
         ]);
     }
 
     // register
-    public function register(Request $request){
-        $register = Validator::make($request->all(),
-        [
-            'name' => 'required|string',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|min:8'
+    public function register(Request $request)
+    {
+        $register = Validator::make($request->all(), [
+            "name" => "required|string",
+            "email" => "required|email|unique:users",
+            "password" => "required|min:8",
         ]);
 
-        if($register->fails()){
+        if ($register->fails()) {
             return response()->json($register->errors());
         }
 
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => bcrypt($request->password),
         ]);
 
-        $accessToken = $user->createToken('accessToken')->accessToken;
+        $accessToken = $user->createToken("accessToken")->accessToken;
 
-        return response()->json([
-            'accessToken' => $accessToken
-        ],200);
+        return response()->json(
+            [
+                "accessToken" => $accessToken,
+            ],
+            200
+        );
     }
 
     // get all user
-    public function users(){
+    public function users()
+    {
         $users = User::all();
         return response()->json($users);
     }
