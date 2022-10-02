049353b4584de07d25a634c58c5e78900a625b47
restu indera <rstx32@gmail.com>
2022-10-02 16:30:53 +0700

    add category controller for web

---
+++ app/Http/Controllers/View/CategoryController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\View;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Article;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class CategoryController extends Controller
+{
+    // show all category
+    public function index()
+    {
+        $categories = Category::where('user_id', Auth::user()->id)->get();
+
+        return view('category.index', ['categories' => $categories]);
+    }
+
+    // show add category form
+    public function create()
+    {
+        return view('category.form-create');
+    }
+
+    // add category
+    public function store(Request $request){
+        $validator = Validator::make($request->all(), [
+            "name" => "required|string",
+        ]);
+
+        if($validator->fails()){
+            return response($validator->errors());
+        }
+
+        Category::create([
+            "name" => $request->name,
+            "user_id" => Auth::user()->id,
+        ]);
+
+        return redirect('/categories');
+    }
+
+    // show list articles of category
+    public function show($id)
+    {
+        $articles = Article::where('user_id', Auth::user()->id)
+        ->where('category_id', $id)
+        ->get();
+
+        return view('category.detail', ['articles' => $articles]);
+    }
+
+    // edit a category
+    public function edit($id)
+    {
+        $categories = Category::where('user_id', Auth::user()->id)->find($id);
+        return view('category.form-edit', ['categories' => $categories]);
+    }
+
+    // update a category
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            "name" => "required|string",
+        ]);
+
+        if($validator->fails()){
+            return response($validator->errors());
+        }
+
+        Category::find($id)->update([
+            "name" => $request->name,
+        ]);
+
+        return redirect('/categories');
+    }
+
+    // delete a category and it's article
+    public function destroy($id){
+        $categories = Category::findOrFail($id);
+        $categories->article->each->delete();
+        $categories->delete();
+
+        return redirect('/categories');
+    }
+}
